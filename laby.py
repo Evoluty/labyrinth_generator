3517110b264672b688eec267c45f734c85a64f50
franck <franck.schmitt@hotmail.com>
2016-12-08 14:41:15 +0100

    stop to randomly chose the wall to open. TODO: need to optimize the number of iterations wich is not correct

--- laby.py
+++ laby.py
@@ -1,4 +1,3 @@
-from tkinter import *
 import numpy as np
 import random
 
@@ -10,12 +9,12 @@ import random
 
 
 # Global var
-laby = None
-wallsVert = None
-wallsHor = None
+lab = None
+ver_walls = None
+hor_walls = None
 
 
-# Check if laby contains only zeros
+# Check if lab contains only zeros
 def contains_only_zeros(matrix):
     for x in range(0, matrix[0].size):
         for y in range(0, matrix[0].size):
@@ -24,76 +23,90 @@ def contains_only_zeros(matrix):
     return True
 
 
-# Open beginning and end of the laby
+# Open beginning and end of the lab
 def open_entrees(size):
-    global wallsVert, wallsHor
+    global ver_walls, hor_walls
 
     if random.random() < 0.5:
         # Open top-bot
         cell_top = random.randint(0, size)
         cell_bot = random.randint(0, size)
-        wallsHor[0, cell_top] = 1
-        wallsHor[size, cell_bot] = 1
+        hor_walls[0, cell_top] = 1
+        hor_walls[size, cell_bot] = 1
     else:
         # Open left-right
         cell_left = random.randint(0, size)
         cell_right = random.randint(0, size)
-        wallsVert[cell_left, 0] = 1
-        wallsVert[cell_right, size] = 1
+        ver_walls[cell_left, 0] = 1
+        ver_walls[cell_right, size] = 1
 
 
 # Initialize lists
 def init_matrix(size):
-    global laby, wallsHor, wallsVert
+    global lab, hor_walls, ver_walls
 
-    # Init values of laby and the other two walls matrix
-    laby = np.fromfunction(lambda i, j: j + size * i, (size, size), dtype=int)
-    wallsVert = np.zeros((size + 1, size + 1), dtype=np.int)
-    wallsHor = np.zeros((size + 1, size + 1), dtype=np.int)
+    # Init values of lab and the other two walls matrix
+    lab = np.fromfunction(lambda i, j: j + size * i, (size, size), dtype=int)
+    ver_walls = np.zeros((size + 1, size + 1), dtype=np.int)
+    hor_walls = np.zeros((size + 1, size + 1), dtype=np.int)
 
     open_entrees(size)
 
 
 # Put the whole path linked to the opened wall to the min value
 def set_path_min_value(c1, c2):
-    global laby
+    global lab
 
-    value = min(laby[c1[0], c1[1]], laby[c2[0], c2[1]])
-    old = max(laby[c1[0], c1[1]], laby[c2[0], c2[1]])
+    value = min(lab[c1[0], c1[1]], lab[c2[0], c2[1]])
+    old = max(lab[c1[0], c1[1]], lab[c2[0], c2[1]])
 
-    for j in range(0, laby[0].size):
-        for i in range(0, laby[0].size):
-            if laby[i, j] == old:
-                laby[i, j] = value
+    for j in range(0, lab[0].size):
+        for i in range(0, lab[0].size):
+            if lab[i, j] == old:
+                lab[i, j] = value
 
 
 # Returns the list of walls that split two paths not connected
 def walls_splitting():
-    global laby, wallsHor, wallsVert
+    global lab, hor_walls, ver_walls
 
     # Left list: vertical walls
     # Right list: horizontal walls
     res = ([], [])
 
-    max_size = laby[0].size - 1
+    max_size = lab[0].size
 
-    for j in range(1, max_size):
+    for j in range(0, max_size):
         for i in range(1, max_size):
-            if laby[i, j] != laby[i-1, j]:
-                res[0].append((i, j))
-            if laby[i, j] != laby[i, j-1]:
-                res[1].append((i, j))
+            if lab[i, j] != lab[i - 1, j]:
+                couple = (i, j)
+                res[0].append(couple)
+            if lab[i, j] != lab[i, j - 1]:
+                couple = (i, j)
+                res[1].append(couple)
     return res
 
 
 # Choose one random wall and opens it then call set_path_min_value function
 def open_next_wall():
-    global laby, wallsHor, wallsVert
+    global lab, hor_walls, ver_walls
 
-    max_size = laby[0].size - 1
+    max_size = lab[0].size - 1
 
-    x = random.randint(0, max_size)
-    y = random.randint(0, max_size)
+    walls = walls_splitting()
+    vertical_walls = walls[0]
+    horizontal_walls = walls[1]
+
+    if (random.random() < 0.5 and vertical_walls) or not horizontal_walls:
+        number = random.randint(0, len(vertical_walls) - 1)
+
+        x = vertical_walls[number][0]
+        y = vertical_walls[number][1]
+    else:
+        number = random.randint(0, len(horizontal_walls) - 1)
+
+        x = horizontal_walls[number][0]
+        y = horizontal_walls[number][1]
 
     c1 = (x, y)
 
@@ -122,55 +135,47 @@ def open_next_wall():
     # TOP
     if direction == 1:
         c2 = (x, y - 1)
-        wallsHor[x, y] = 1
+        hor_walls[x, y] = 1
     # RIGHT
     elif direction == 2:
         c2 = (x + 1, y)
-        wallsVert[x+1, y] = 1
+        ver_walls[x + 1, y] = 1
     # BOTTOM
     elif direction == 3:
         c2 = (x, y + 1)
-        wallsHor[x, y+1] = 1
+        hor_walls[x, y + 1] = 1
     # LEFT
     else:
         c2 = (x - 1, y)
-        wallsVert[x, y] = 1
+        ver_walls[x, y] = 1
 
     set_path_min_value(c1, c2)
 
-    print(laby)
-    print("")
-    print(wallsVert)
-    print("")
-    print(wallsHor)
+    print(lab)
     print("")
 
 
 # Draw the lab with tkinter
 def draw_lab():
-    global laby, wallsHor, wallsVert
+    global lab, hor_walls, ver_walls
     pass
 
 
 # Main function of the program
 def main():
-    global laby, wallsHor, wallsVert
+    global lab, hor_walls, ver_walls
 
-    size = int(input("Enter the size of the labyrinth: "))
-    init_matrix(size)
+    # size = int(input("Enter the size of the labyrinth: "))
+    # init_matrix(size)
+    init_matrix(5)
 
     i = 0
-    while not(contains_only_zeros(laby)):
+    while not (contains_only_zeros(lab)):
+        print(i)
         open_next_wall()
         i += 1
 
     draw_lab()
-
-    print(laby)
-    print("")
-    print(wallsVert)
-    print("")
-    print(wallsHor)
 
 
 # Launch main when file is called
